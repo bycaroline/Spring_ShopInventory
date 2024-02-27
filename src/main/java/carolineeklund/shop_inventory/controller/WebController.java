0dbe9e01f2e685ec136050bfc96d0ad8eb00a8de
bycaroline <eklund.caroline@gmail.com>
2024-02-27 16:50:33 -0700

    feature/styling commit

--- src/main/java/carolineeklund/shop_inventory/controller/WebController.java
+++ src/main/java/carolineeklund/shop_inventory/controller/WebController.java
@@ -2,17 +2,27 @@ package carolineeklund.shop_inventory.controller;
 
 import carolineeklund.shop_inventory.model.Item;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Controller class responsible for handling web requests related to the shop.
+ */
 @Controller
 public class WebController {
-
-    private final String SHOP_CONTROLLER_BASE_URL = "http://localhost:8087"; // Adjust this URL accordingly
+    // Base URL for the shop controller
+    private final String SHOP_CONTROLLER_BASE_URL = "http://localhost:8087";
+    // Instance of RestTemplate to make HTTP requests
     private final RestTemplate restTemplate;
 
+    /**
+     * Constructor for WebController.
+     *
+     * @param restTemplate Instance of RestTemplate to inject.
+     */
     @Autowired
     public WebController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
@@ -24,18 +34,18 @@ public class WebController {
         return "home";
     }
 
-
-
     @GetMapping("/about")
     public String about(Model model) {
-        // Call the findAll endpoint of ShopController to fetch items
         Item[] items = restTemplate.getForObject(SHOP_CONTROLLER_BASE_URL + "/api/shop", Item[].class);
-
-        // Add the items to the model
         model.addAttribute("items", items);
+
+//        Item [] lowQuantityItems = restTemplate.getForObject(SHOP_CONTROLLER_BASE_URL+ "/api/shop" + "/search/quantity", Item[].class);
+//        model.addAttribute("lowQuantityItems", lowQuantityItems );
+
+        ResponseEntity<Item[]> response = restTemplate.getForEntity(SHOP_CONTROLLER_BASE_URL + "/api/shop/search/quantity", Item[].class);
+        Item[] lowQuantityItems = response.getBody();
+        model.addAttribute("lowQuantityItems", lowQuantityItems);
 
         return "about";
     }
-
-
 }
